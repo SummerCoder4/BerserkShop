d9ae4d36f20b0a1a967e3729c672e40e314013df
JulesCharles <julees.charles@gmail.com>
2022-02-03 20:43:54 +0100

    ajout link poster home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,32 @@
 import vikingsLogo from '../assets/vikingsLogo.png'
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FcSearch} from 'react-icons/fc';
+import { Link } from "react-router-dom"
 
 import './Header.css'
 
 const Header = () => {
     return (
         <div className="headerContainer">
+            <Link to='/'>
             <div className="holderLogo">
                 <img className="logo" src={vikingsLogo} alt="Logo" />
             </div>
+            </Link>
             <div className="holderTitle">
                 <h1>BERSERK SHOP .</h1>
             <div className="holderSearch">
-                  
                 <div className="searchBox"><GiHamburgerMenu className="holderBurger" />Catégories</div>  
                 <div className="searchBox"><FcSearch/> <input className="holderZoom" type="text" name="text" class="search" placeholder="Recherchez ici!"/></div>
                 <div id='searchButton'>Rechercher ()</div>
             </div>
             </div>
+            
             <div className="firstButton">
+             <Link to='/Poster'>
                 <div className="firstButtonStyle">Poster son annonce</div>
+             </Link>
+                
             </div> 
                  
         </div>
