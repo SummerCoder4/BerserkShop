3aae754bc45ca51d4b72b6e42256e9166aaeacc5
MatthiasTeks <mathias.vimbert@gmail.com>
2022-02-04 10:22:46 +0100

    implemente post modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Link } from "react-router-dom"
 import './Header.css'
+import { useState } from "react";
 
 const Header = ({ data, setFilterData, mapThis, handleSearch }) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="headerContainer">
             <Link to='/'>
@@ -40,9 +42,43 @@ const Header = ({ data, setFilterData, mapThis, handleSearch }) => {
                 </div>
             </div>
             <div className="firstButton">
-                <Link to='/Poster'>
-                    <div className="firstButtonStyle">Poster son annonce</div>
-                </Link>
+                <button className="firstButtonStyle" onClick={() => setIsOpen(!isOpen)}>
+                    Poster son annonce
+                </button>
+                <div className={`containerModal ${isOpen ? "active" : ""}`}>
+                    <div className="headerPoster">
+                        <h1 className="berk">
+                            BERKSERK SHOP.
+                            <button className="croix" onClick={() => setIsOpen(!isOpen)}>
+                                X
+                            </button>
+                            <button className="onglet">
+                                Déposer une annonce
+                            </button>
+                        </h1>
+                    </div>
+                    <div className="holderContainer">
+                        <h2>Commençons par l'essentiel!</h2>
+                        <div className="titre">
+                            <p2>Quel est le titre de l'annonce?</p2>
+                            <div className="reponse">
+                                <input className="searchs" type="text" name="text"></input>
+                                <div className="catégorie">
+                                    <p3>Choisissez la catégorie du produit</p3>
+                                    <div className="reponse">
+                                        <input className="searchs" type="text" name="text"></input>
+                                    </div>
+                                    <div className="prix">
+                                        <p4>Entrez le prix</p4>
+                                        <div className="reponse">
+                                            <input className="searchs" type="text" name="text"></input>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
